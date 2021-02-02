57e0cfc04f7eebabc006b4d5753f2ffe91608d7c
ElianaDG <58051386+ElianaDG@users.noreply.github.com>
2021-02-02 18:43:26 -0500

    Added Rocket and RocketTest and Main

---
+++ src/Rocket.java
@@ -0,0 +1,44 @@
+public class Rocket {
+
+    final double GRAVITY = 9.8;
+    private final double velocity;
+    private final double angle;
+
+    public Rocket(double velocity, double angle) {
+        this.velocity = velocity;
+        this.angle = angle;
+    }
+
+    /**
+     *
+     * @param time in seconds
+     * @return the x location of the rocket at the specified time
+     */
+    public double getX(double time) {
+        double xVelocity = Math.cos(Math.toRadians(angle)) * velocity;
+        double x = xVelocity * time;
+        return x;
+    }
+
+    /**
+     *
+     * @param time
+     * @return the y location of the rocket at the specified time
+     */
+    public double getY(double time) {
+        double yVelocity = Math.sin(Math.toRadians(angle)) * velocity;
+        double y = (yVelocity * time) - (0.5 * GRAVITY * time * time);
+        return y;
+    }
+
+    /**
+     *
+     * @return the time in seconds that the rocket lands
+     */
+    public double getTimeToLand(){
+        double yVelocity = Math.sin(Math.toRadians(angle)) * velocity;
+        double timeToLand = 2 * yVelocity / GRAVITY;
+        return timeToLand;
+    }
+
+}
---
+++ src/RocketTest.java
@@ -0,0 +1,43 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RocketTest {
+
+    @Test
+    public void getX(){
+        //given
+        Rocket rocket = new Rocket(71, 35);
+
+        //when
+        double x = rocket.getX(3);
+
+        //then
+        Assert.assertEquals(174.47, x, 0.01);
+    }
+
+    @Test
+    public void getY(){
+        //given
+        Rocket rocket = new Rocket(71, 35);
+
+        //when
+        double y = rocket.getY(3);
+
+        //then
+        Assert.assertEquals(78.07, y, 0.01);
+    }
+
+    @Test
+    public void timeToLand(){
+        //given
+        Rocket rocket = new Rocket(71, 35);
+
+        //when
+        double timeToLand = rocket.getTimeToLand();
+
+        //then
+        Assert.assertEquals(8.31, timeToLand, 0.01);
+    }
+
+
+}

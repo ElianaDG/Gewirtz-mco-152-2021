13121a6ebd2248fbaaa1c8e6695fb092c9d861c0
ElianaDG <58051386+ElianaDG@users.noreply.github.com>
2021-03-02 19:05:41 -0500

    changed the Dictionary test

--- src/test/java/gewirtz/scrabble/DictionaryTest.java
+++ src/test/java/gewirtz/scrabble/DictionaryTest.java
@@ -32,4 +32,29 @@ public class DictionaryTest {
         Assert.assertTrue(wordTest);
     }
 
+    @Test
+    public void testValidDefinition()
+    {
+        //given
+        Dictionary dictionary = new Dictionary("dictionary.txt");
+
+        //when
+        String definitionTest = dictionary.getDefinition("liability");
+
+        //then
+        Assert.assertEquals("", definitionTest);
+    }
+
+    @Test
+    public void testInvalidDefinition()
+    {
+        //given
+        Dictionary dictionary = new Dictionary("dictionary.txt");
+
+        //when
+        String definitionTest = dictionary.getDefinition("skjhgfrd");
+
+        //then
+        Assert.assertEquals("", definitionTest);
+    }
 }
